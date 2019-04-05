9257fe1907f68d77be3111b78d39e770616f25d5
anamariaagudelo <anamariaagudeloisi@gmail.com>
2019-04-05 10:30:54 -0500

    Vista TableRepos ok

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Router from '../../screens/Router'
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 
 
--- src/screens/RepositoriesCandidate/TableRepos.js
+++ src/screens/RepositoriesCandidate/TableRepos.js
@@ -17,31 +17,6 @@ class TableRepos extends Component {
             hidePageListOnlyOnePage: true
         };
 
-
-        // const columns = [{
-        //     dataField: 'name',
-        //     text: 'NOMBRE',
-        //     filter: textFilter(),
-        //     sort: true
-        // }, {
-        //     dataField: 'description',
-        //     text: 'DESCRIPCIÓN',
-        //     sort: true
-        // }, {
-        //     dataField: 'git_url',
-        //     text: 'URL_GIT',
-        //     sort: true
-        // },
-        // {
-        //     dataField: 'default_branch',
-        //     text: 'DEFAULT_BRANCH',
-        //     sort: true
-        // }, {
-        //     dataField: 'language',
-        //     text: 'LANGUJE',
-        //     sort: true
-        // }];
-
         const repos = R.pathOr([], ['repos'])(this.props)
 
         const message = repos.length ? '' : 'No Repositories'
@@ -51,23 +26,20 @@ class TableRepos extends Component {
                 <h2 className="text-center">{message}</h2>
                 <div className="card border-primary text-white bg-dark mb-3 overflow-auto" >
                     <div className="card-body">
-                        <BootstrapTable striped bordered hover variant="dark" overflow-auto 
+                        <BootstrapTable striped bordered hover variant="dark" overflow-auto
                             data={repos} 
                             keyField="id"
                             options={options}
                             pagination
                             search 
                             searchPlaceholder='Filtrar'
-                            /* // columns={columns}
-                            // pagination={paginationFactory(options)}
-                            // filter={filterFactory()}
-                            // defaultSortDirection="asc" */
+                            headerStyle={ { background: '#eeee' } }
                         >
-                        <TableHeaderColumn dataField='name' dataSort={ true }>NOMBRE</TableHeaderColumn>
-                        <TableHeaderColumn dataField='description' dataSort={ true }>DESCRIPCIÓN</TableHeaderColumn>
-                        <TableHeaderColumn dataField='git_url' dataSort={ true }>URL_GIT</TableHeaderColumn>
-                        <TableHeaderColumn dataField='default_branch' dataSort={ true }>DEFAULT_BRANCH</TableHeaderColumn>
-                        <TableHeaderColumn dataField='language' dataSort={ true }>LENGUJE</TableHeaderColumn>
+                            <TableHeaderColumn dataField='name' dataSort={ true }>NOMBRE</TableHeaderColumn>
+                            <TableHeaderColumn dataField='description' dataSort={ true }>DESCRIPCIÓN</TableHeaderColumn>
+                            <TableHeaderColumn dataField='git_url' dataSort={ true }>URL_GIT</TableHeaderColumn>
+                            <TableHeaderColumn dataField='default_branch' dataSort={ true }>DEFAULT_BRANCH</TableHeaderColumn>
+                            <TableHeaderColumn dataField='language' dataSort={ true }>LENGUJE</TableHeaderColumn>
                         </BootstrapTable>       
                     </div>
                 </div>
